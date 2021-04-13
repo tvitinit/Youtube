82984d886fb6f07f4c773997dfe8f5e8e548a456
tvitinit <81964928+tvitinit@users.noreply.github.com>
2021-04-13 21:12:09 +0100

    Added Static Methods

---
+++ StaticMethods/StaticMethods/WordStatistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMethods
+{
+	public class WordStatistics
+	{
+		public int TotalWordsAndSentences { get; set; }
+
+		public int AverageWordCountInSentence { get; set; }
+
+		public int AverageWordLength { get; set; }
+
+		public override string ToString()
+		{
+			return $@"Total words and sentences: {TotalWordsAndSentences}
+Average Word Count in Sentence: {AverageWordCountInSentence}
+Average Word Length: {AverageWordLength}";
+		}
+	}
+}
---
+++ StaticMethodsFinal/StaticMethods/WordStatisticsCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMethods
+{
+	public class WordStatisticsCalculator
+	{
+		private readonly ICalculatorHelpers _calculatorHelpers;
+
+		public WordStatisticsCalculator(ICalculatorHelpers calculatorHelpers)
+		{
+			_calculatorHelpers = calculatorHelpers;
+		}
+
+		public WordStatistics CalculateStatistics(string text)
+		{
+			var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			var sentences = text.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+			var stats = new WordStatistics
+			{
+				AverageWordCountInSentence = _calculatorHelpers.CalculateAverageWordCountInSentence(sentences),
+				AverageWordLength = _calculatorHelpers.CalculateAverageWordLength(words),
+				TotalWords = _calculatorHelpers.CalculateTotalWords(words)
+			};
+
+			return stats;
+		}
+
+	}
+}
